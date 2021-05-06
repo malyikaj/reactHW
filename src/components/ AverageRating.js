1c525f800488150b885283b17206312987cd8d25
Malyikanima Jackson <malyikasmac@MacBook-Pro.home>
2021-05-06 17:25:06 -0400

    First commit

---
+++ src/components/ AverageRating.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function  AverageRating() {
+    return (
+        <div className="rating">
+            <h1>Average Rating</h1>
+        </div>
+    )
+}
+
+export default  AverageRating
---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Sidebar = () => {
+    return(
+        <div>
+            <p>Sidebar:</p>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+        </div>
+        
+    );
+}
+
+export default Sidebar
