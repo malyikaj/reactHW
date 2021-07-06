a033dce9084c65efc0afbadcfe9dd44c031008de
Malyika J <62891809+malyikaj@users.noreply.github.com>
2021-07-05 23:06:50 -0400

    updating Home component with about us link

--- mod2project/src/components/Home.js
+++ mod2project/src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
        
         <h2>Welcome to Astrology Center!</h2>
         <p>Check out our <strong><a href="/DailyHoroscopes"target="_blank">Daily Horoscopes</a></strong> to see what future has in store for you today! Complete with </p>
-        <p>Click on our <strong>About</strong> to find out where we sourced our content.</p>
+        <p>Click on our <strong><a href="/AboutUs">About</a></strong> to find out where we sourced our content.</p>
         <hr />
       </div>
     );
